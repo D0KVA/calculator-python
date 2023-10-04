96ded245dab9e66fd1910a7b712c92f53dfaffdb
D0KVA <144143826+D0KVA@users.noreply.github.com>
2023-10-04 17:03:44 +0300

    переделал код полностью, он полностью уникальный, если нет, то съем мыло

--- calculatorpython.py
+++ calculatorpython.py
@@ -1,5 +1,9 @@
 import math
-while True:
+
+zxc = 0  
+rez = 0  
+
+while zxc != 11:
     print("Выберите операцию:")
     print("1. Сложение")
     print("2. Вычитание")
@@ -8,49 +12,71 @@ while True:
     print("5. Возведение в степень")
     print("6. Квадратный корень")
     print("7. Факториал")
-    print("8. sin")
-    print("9. cos")
-    print("10. tg")
-    print("11. Exit")
-    zxc = input("Введите номер операции: ")
-    if zxc == '11':
-        print("Конец.")
-        break
-    if zxc in ('1', '2', '3', '4', '5'):
-        a = float(input("Введите первое число: "))
-        b = float(input("Введите второе число: "))
-        if zxc == '1':
-            result = a + b
-        elif zxc == '2':
-            result = a - b
-        elif zxc == '3':
-            result = a * b
-        elif zxc == '4':
-            if b == 0:
-                print("Деление на 0 невозможно.")
-                continue
-            result = a / b
-        elif zxc == '5':
-            result = a ** b
-    elif zxc in ('6', '7', '8', '9', '10'):
-        c = float(input("Введите число: "))
-        if zxc == '6':
-            if c < 0:
+    print("8. Синус")
+    print("9. Косинус")
+    print("10. Тангенс")
+    print("11. Exit")  
+    
+    try:
+        zxc = int(input("Введите номер операции: ")) 
+        if zxc > 11:
+            print("Ошибка: Введена неверная операция.")
+            continue      
+        elif zxc == 1:
+            num1 = float(input("Введите первое число: "))
+            num2 = float(input("Введите второе число: "))
+            rez = num1 + num2
+            print(f"Результат: {rez}")
+        elif zxc == 2:
+            num1 = float(input("Введите первое число: "))
+            num2 = float(input("Введите второе число: "))
+            rez = num1 - num2
+            print(f"Результат: {rez}")
+        elif zxc == 3:
+            num1 = float(input("Введите первое число: "))
+            num2 = float(input("Введите второе число: "))
+            rez = num1 * num2
+            print(f"Результат: {rez}")
+        elif zxc == 4:
+            num1 = float(input("Введите первое число: "))
+            num2 = float(input("Введите второе число: "))
+            if num2 == 0:
+                print("Ошибка: деление на 0.")
+            else:
+                rez = num1 / num2
+        elif zxc == 5:
+            num1 = float(input("Введите первое число: "))
+            num2 = float(input("Введите второе число: "))
+            rez = num1 ** num2
+            print(f"Результат: {rez}")
+        elif zxc == 6:
+            num = float(input("Введите число: "))
+            if num < 0:
                 print("Квадратный корень отрицательного числа не может быть.")
-                continue
-            result = math.sqrt(c)
-        elif zxc == '7':
-            if c < 0:
-                print("Факториал отрицательного числа не можнт быть.")
-                continue
-            result = math.factorial(int(c))
-        elif zxc == '8':
-            result = math.sin(c)
-        elif zxc == '9':
-            result = math.cos(c)
-        elif zxc == '10':
-            result = math.tan(c)
-    else:
-        print("Неверный выбор операции.")
-        continue
-    print(f"Результат: {result}")
+            else:
+                rez = math.sqrt(num)
+                print(f"Результат: {rez}")
+        elif zxc == 7:
+            num = float(input("Введите число: "))
+            if num < 0:
+                print("Факториал отрицательного числа не может быть.")
+            else:
+                rez = math.factorial(int(num))
+                print(f"Результат: {rez}")
+        elif zxc == 8:
+            num = float(input("Введите число: "))
+            rez = math.sin(num)
+            print(f"Результат: {rez}")
+        elif zxc == 9:
+            num = float(input("Введите число: "))
+            rez = math.cos(num)
+            print(f"Результат: {rez}")
+        elif zxc == 10:
+            num = float(input("Введите число: "))
+            rez = math.tan(num)
+            print(f"Результат: {rez}")
+        elif zxc == 11:
+            print("Выход из калькулятора.")
+            break
+    except ValueError:
+        print("Ошибка: Неверная операция.")
